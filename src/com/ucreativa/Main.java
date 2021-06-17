543ad9f353de94f11ba0fdd816f238135f29d6a0
Bryan <bryan.guzman@avantica.com>
2021-06-17 10:21:23 -0600

    Agregando mas logica y probando la base de datos con los respectivos campos

--- src/com/ucreativa/Main.java
+++ src/com/ucreativa/Main.java
@@ -1,6 +1,7 @@
 package com.ucreativa;
 
 import com.ucreativa.delivery.entities.Express;
+import com.ucreativa.delivery.entities.PickUp;
 import com.ucreativa.delivery.entities.Producto;
 import com.ucreativa.delivery.repositories.FileRepository;
 import com.ucreativa.delivery.repositories.Repository;
@@ -23,7 +24,7 @@ public class Main {
                 // aca vienen los campos de la clase express
                 direccionEnvio, costoEnvio, tiempoEnvio,
                 // aca vienen los campos de la clase pickup
-                horaEntrega, pedidoOnline, pedidoLlamada;
+                horaEntrega, pedidoOnline, pedidoLlamada, tipo;
 
 
         boolean bandera = true;
@@ -58,8 +59,39 @@ public class Main {
             System.out.println("Metodo de pago:");
             metodoPago = scan.nextLine();
 
+            if(tipoEnvio.equals("E")){
 
+                System.out.println("Direccion de envio express:");
+                direccionEnvio = scan.nextLine();
 
+                System.out.println("Costo de envio a esa direccion:");
+                costoEnvio = scan.nextLine();
+
+                System.out.println("Tiempo de envio a esa direccion:");
+                tiempoEnvio = scan.nextLine();
+
+                producto = new Express(Integer.parseInt(id), nombreProducto, nombreRestaurante, Double.parseDouble(precio),
+                        tipoEnvio, nombreCliente, telefonoCliente, metodoPago, direccionEnvio, Double.parseDouble(costoEnvio), Integer.parseInt(tiempoEnvio));
+
+                tipo = "E";
+
+            }else{
+
+                System.out.println("Minutos a partir de ahora para la entrega:");
+                horaEntrega = scan.nextLine();
+
+                System.out.println("El pedido fue online? (S/N):");
+                pedidoOnline = scan.nextLine();
+
+                System.out.println("El pedido fue por llamada? (S/N):");
+                pedidoLlamada = scan.nextLine();
+
+                producto = new PickUp(Integer.parseInt(id), nombreProducto, nombreRestaurante, Double.parseDouble(precio), tipoEnvio, nombreCliente, telefonoCliente, metodoPago, Integer.parseInt(horaEntrega), pedidoLlamada.equals("S"), pedidoOnline.equals("S"));
+
+                tipo = "P";
+            }
+
+            repo.guardarProducto(producto, tipo);
 
             System.out.println("Desea imprimir la lista (S)");
             String print = scan.nextLine();
